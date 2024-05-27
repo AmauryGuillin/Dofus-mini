2a2a73361971e51fa0b87e7830b0082fb95c53d4
AmauryGuillin <amauryg@hotmail.fr>
2024-05-27 21:37:03 +0200

    fix: cell selection on playing-board component

--- src/components/playing-board.tsx
+++ src/components/playing-board.tsx
@@ -2,58 +2,44 @@ import { useState } from "react";
 import { Board } from "../types/board";
 
 export default function PlayingBoard() {
-  const [targetedCell, setTargetedCell] = useState<number>();
+  const [targetedCell, setTargetedCell] = useState<string>();
   const board: Board = {
     name: "main",
     width: 4,
     length: 4,
   };
 
-  function selectCell(cell: number) {
-    setTargetedCell(cell);
+  function selectCell(key: string) {
+    setTargetedCell(key);
     console.log(targetedCell);
   }
 
-  const grid = [board.width, board.length];
-
-  return (
-    <>
-      <div className="w-full h-screen border-2">
-        <div className="text-white transform-gpu rotate-[30deg] -skew-x-[30deg]"></div>
-      </div>
-    </>
+  const grid = Array.from({ length: board.length }, (_, rowIndex) =>
+    Array.from({ length: board.width }, (_, colIndex) => {
+      const key = `${rowIndex}-${colIndex}`;
+      return (
+        <div
+          key={key}
+          className={`w-24 h-24 border-2 border-gray-500 hover:bg-gray-400 ${
+            key === targetedCell ? "bg-red-500" : undefined
+          }`}
+          onClick={() => selectCell(key)}
+        />
+      );
+    })
   );
 
-  //   const grid = Array.from({ length: board.length }, (_, rowIndex) =>
-  //     Array.from({ length: board.width }, (_, colIndex) => (
-  //       <div
-  //         key={`${rowIndex}-${colIndex}`}
-  //         className="w-24 h-24 border-2 border-gray-500 hover:bg-gray-400"
-  //       />
-  //     ))
-  //   );
-
-  //   return (
-  //     <div className="text-white transform-gpu rotate-[30deg] -skew-x-[30deg]">
-  //       {grid.map((row, rowIndex) =>
-  //         rowIndex === targetedCell ? (
-  //           <div
-  //             key={rowIndex}
-  //             className="flex bg-red-500"
-  //             onClick={() => selectCell(rowIndex)}
-  //           >
-  //             {row}
-  //           </div>
-  //         ) : (
-  //           <div
-  //             key={rowIndex}
-  //             className="flex"
-  //             onClick={() => selectCell(rowIndex)}
-  //           >
-  //             {row}
-  //           </div>
-  //         )
-  //       )}
-  //     </div>
-  //   );
+  return (
+    <div className="h-screen flex justify-center items-center border-2 w-full">
+      <div className="text-white transform-gpu rotate-[30deg] -skew-x-[30deg]">
+        {grid.map((row, rowIndex) => {
+          return (
+            <div key={rowIndex} className="flex">
+              {row}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
