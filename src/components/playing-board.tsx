eddd960eb780f4dbe3db8e465adda85f6e1b0219
AmauryGuillin <amauryg@hotmail.fr>
2024-05-28 18:08:41 +0200

    feat: adding player pm moving limitation (with PM) and some UI

--- src/components/playing-board.tsx
+++ src/components/playing-board.tsx
@@ -1,34 +1,39 @@
 import { useEffect, useState } from "react";
 import { Board } from "../types/board";
+import { Player } from "../types/player";
+import PlayerInfo from "./player-info";
 
 export default function PlayingBoard() {
   const [targetedCell, setTargetedCell] = useState<string>();
   const [path, setPath] = useState<string[]>([]);
+
   const board: Board = {
     name: "main",
     width: 4,
     length: 4,
   };
 
+  const player: Player = {
+    name: "PLAYER_NAME",
+    pv: 100,
+    pm: 3,
+    pa: 6,
+  };
+
   useEffect(() => {
     setTargetedCell("3-0");
   }, []);
 
-  function calculatePath(start: string, end: string): string[] {
-    //transforme la position de départ en deux nombres distincs "startRow" et "startCol" rangé dans un tableau
+  function calculatePath(
+    start: string,
+    end: string,
+    maxLength: number
+  ): string[] {
     const [startRow, startCol] = start.split("-").map(Number);
-
-    //transforme la position d'arrivée en deux nombres distincs "endRow" et "endCol" rangé dans un tableau
     const [endRow, endCol] = end.split("-").map(Number);
-
     const queue = [[startRow, startCol]];
-
-    //chaque élément d'un "Set" doit être unique
     const visited = new Set();
-
-    //collection sous forme de clé-valeur
     const cameFrom = new Map();
-
     const directions = [
       [0, 1], // right
       [1, 0], // down
@@ -36,32 +41,26 @@ export default function PlayingBoard() {
       [-1, 0], // up
     ];
 
-    //on ajoute une nouvelle valeur de position à visited -> la valeur de départ
     visited.add(`${startRow}-${startCol}`);
 
-    //tant que queue contient une valeur
     while (queue.length > 0) {
-      //on indique dans row et col, les prochaines valeurs de la queue
       const [row, col] = queue.shift()!;
 
-      //si les valeurs de row et col actuelles sont égales aux valeur finales (voulues par l'utilisateur)
       if (row === endRow && col === endCol) {
-        //on set le path à "vide"
         const path = [];
-
-        //on dit dur current = les valeurs finales
         let current = `${endRow}-${endCol}`;
 
-        //tant que la valeur de current est différente de celle de départ
         while (current !== start) {
-          //on ajoute au path les valeurs finales
           path.push(current);
-
-          //met à jour l'historique du chemin pour reconstruire le chemin final
           current = cameFrom.get(current);
         }
         path.push(start);
-        return path.reverse();
+
+        if (path.length - 1 <= maxLength) {
+          return path.reverse();
+        } else {
+          return [];
+        }
       }
 
       for (const [dRow, dCol] of directions) {
@@ -91,7 +90,7 @@ export default function PlayingBoard() {
   }
 
   function enter(key: string) {
-    const newPath = calculatePath(targetedCell!, key);
+    const newPath = calculatePath(targetedCell!, key, player.pm);
     setPath(newPath);
   }
 
@@ -105,7 +104,7 @@ export default function PlayingBoard() {
       return (
         <div
           key={key}
-          className={`w-24 h-24 border-2 border-gray-500 hover:bg-green-500 ${
+          className={`w-24 h-24 border-2 border-gray-500 hover:cursor-pointer ${
             key === targetedCell
               ? "bg-red-500"
               : path.includes(key)
@@ -121,8 +120,9 @@ export default function PlayingBoard() {
   );
 
   return (
-    <div className="h-screen flex justify-center items-center border-2 w-full">
-      <div className="text-white transform-gpu rotate-[30deg] -skew-x-[30deg]">
+    <div className="h-screen flex justify-center items-center border-2 w-full text-white relative">
+      <PlayerInfo player={player} />
+      <div className="transform-gpu rotate-[30deg] -skew-x-[30deg]">
         {grid.map((row, rowIndex) => {
           return (
             <div key={rowIndex} className="flex">
