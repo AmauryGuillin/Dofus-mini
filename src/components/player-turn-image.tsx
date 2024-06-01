bf4f2b5c669d228a57e6cf0235899d8d3174423c
AmauryGuillin <amauryg@hotmail.fr>
2024-06-01 15:22:30 +0200

    feat: prettier animation on turn images

--- src/components/player-turn-image.tsx
+++ src/components/player-turn-image.tsx
@@ -6,13 +6,17 @@ type Props = {
 
 export default function PlayerTurnImage({ player }: Props) {
   return (
-    <div className="absolute top-4 left-8 w-48 h-[253px] bg-gray-300 rounded-lg transition-all animate-slideInFromLeft">
-      <div className="text-black font-bold text-xl pl-2">{player.name}</div>
-      {player.name !== "Bouftou" ? (
-        <img src="./images/1.png" />
-      ) : (
-        <img src="./images/bouftou.png" />
-      )}
+    <div className="absolute top-4 left-8 w-48 h-[253px] bg-gray-300 rounded-lg">
+      <div className="animate-slideInFromLeft delay-500">
+        <div className="text-black font-bold text-xl pl-2">{player.name}</div>
+      </div>
+      <div className="animate-slideInFromLeft">
+        {player.name !== "Bouftou" ? (
+          <img src="./images/1.png" className="animate-slideInFromLeft" />
+        ) : (
+          <img src="./images/bouftou.png" className="animate-slideInFromLeft" />
+        )}
+      </div>
     </div>
   );
 }
