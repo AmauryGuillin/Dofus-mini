0e1126b143870c3dae143e550a0afb4fa5dceac0
AmauryGuillin <amauryg@hotmail.fr>
2024-07-13 15:29:06 +0200

    fix: adjusting HUD size

--- src/components/player-info.tsx
+++ src/components/player-info.tsx
@@ -23,23 +23,23 @@ export default function PlayerInfo() {
           <Circle
             strokeWidth={1.25}
             stroke="blue"
-            className="absolute top-14 -left-[70px] -translate-x-1/2 -translate-y-1/2 size-16"
+            className="absolute top-14 -left-[95px] -translate-x-1/2 -translate-y-1/2 size-16"
           />
-          <span className="absolute top-14 -left-[71px] -translate-x-1/2 -translate-y-1/2 font-semibold text-2xl text-blue-600">
+          <span className="absolute top-14 -left-[96px] -translate-x-1/2 -translate-y-1/2 font-semibold text-2xl text-blue-600">
             {player.pa}
           </span>
-          <span className="absolute top-20 -left-[105px] -translate-x-1/2 -translate-y-1/2 font-semibold text-2xl text-blue-600">
+          <span className="absolute top-20 -left-[131px] -translate-x-1/2 -translate-y-1/2 font-semibold text-2xl text-blue-600">
             PA
           </span>
           <Circle
             strokeWidth={1.25}
             stroke="green"
-            className="absolute top-32 -left-[115px] -translate-x-1/2 -translate-y-1/2 size-16"
+            className="absolute top-32 -left-[141px] -translate-x-1/2 -translate-y-1/2 size-16"
           />
-          <span className="absolute top-32 -left-[115px] -translate-x-1/2 -translate-y-1/2 font-semibold text-2xl text-green-500">
+          <span className="absolute top-32 -left-[141px] -translate-x-1/2 -translate-y-1/2 font-semibold text-2xl text-green-500">
             {player.pm}
           </span>
-          <span className="absolute top-40 -left-[150px] -translate-x-1/2 -translate-y-1/2 font-semibold text-2xl text-green-500">
+          <span className="absolute top-40 -left-[176px] -translate-x-1/2 -translate-y-1/2 font-semibold text-2xl text-green-500">
             PM
           </span>
         </div>
