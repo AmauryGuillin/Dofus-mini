11f270ef053c5482f85f742aa60bf06b5711acaa
Florian Guillin <FGuillin@union-des-forgerons.fr>
2024-07-23 21:09:08 +0200

    refactor: :lipstick: update player`s HUD to fit in multiple screen size

--- src/hooks/usePlayingBoard.tsx
+++ src/hooks/usePlayingBoard.tsx
@@ -94,7 +94,7 @@ export function usePlayingBoard(
       return (
         <div
           key={key}
-          className={`size-[3.5dvw] border-2 border-gray-500 hover:cursor-pointer ${
+          className={`size-[3.5dvw] 2xl:size-[2.5dvw] border-2 border-gray-500 hover:cursor-pointer ${
             key === playerCell
               ? "border-2 relative"
               : key === enemyCell
