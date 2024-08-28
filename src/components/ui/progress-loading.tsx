fc0a008791539e1dba9d48cdf6d81f92d9a523fa
AmauryGuillin <amauryg@hotmail.fr>
2024-08-29 01:30:10 +0200

    style: :lipstick: styling title screen

--- src/components/ui/progress-loading.tsx
+++ src/components/ui/progress-loading.tsx
@@ -21,7 +21,7 @@ const ProgressLoading = React.forwardRef<
         transform: `translateX(-${100 - ((value || 0) / max!) * 100}%)`,
       }}
     />
-    <div className=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white font-semibold">
+    <div className=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white font-bold sm:text-sm">
       {content} {value}%
     </div>
   </ProgressPrimitive.Root>
