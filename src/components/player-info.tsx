1fd6974e3331e8a60873b8af002c558d1e6025c7
AmauryGuillin <amauryg@hotmail.fr>
2024-09-25 00:01:32 +0200

    feat: :sparkles: player health display on the heart is now 'color dynamic'

--- src/components/player-info.tsx
+++ src/components/player-info.tsx
@@ -1,7 +1,7 @@
 import { useStore } from "@/hooks/store";
 import { playClickSounds } from "@/utils/music/handleAudio";
 import { Circle, Heart } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Avatar, AvatarImage } from "./ui/avatar";
 import { Separator } from "./ui/separator";
 
@@ -10,6 +10,27 @@ export default function PlayerInfo() {
   const healthPercentage = Math.round(
     Math.min((player.pv / player.pvMax) * 100, 100)
   );
+
+  const [healthParamFirst, setHealthParamFirst] = useState<number>(0);
+  const [healthParamSecond, setHealthParamSecond] = useState<number>(0);
+
+  useEffect(() => {
+    if (healthPercentage >= 50) {
+      const t =
+        Math.round(Math.min((player.pv / 2 / (player.pvMax / 2)) * 100, 100)) -
+        100;
+      setHealthParamFirst(t);
+    }
+  }, [healthPercentage]);
+
+  useEffect(() => {
+    if (healthPercentage < 50) {
+      const t =
+        Math.round(Math.min((player.pv / player.pvMax) * 100, 100)) - 50;
+      setHealthParamSecond(t);
+      setHealthParamFirst(-50);
+    }
+  }, [healthPercentage, healthParamSecond]);
 
   const [healthDisplay, setHealthDisplay] = useState<number>(0);
 
@@ -20,6 +41,18 @@ export default function PlayerInfo() {
 
   return (
     <>
+      <svg width="0" height="0">
+        <linearGradient
+          id="health-gradient"
+          x1="100%"
+          y1={`${healthPercentage > 0 ? `${-healthParamSecond * 2}%}` : "99%"}`}
+          x2="100%"
+          y2={`${healthPercentage > 0 ? `${-healthParamFirst * 2}%` : "100%"}`}
+        >
+          <stop stopColor="#FFFF66" offset="50%" />
+          <stop stopColor="#D60201" offset="50%" />
+        </linearGradient>
+      </svg>
       <div className="relative">
         <Avatar className="h-auto w-44 border-4 shrink flex items-center justify-center">
           <AvatarImage src="./images/1.png" className="h-auto max-w-full" />
@@ -29,23 +62,24 @@ export default function PlayerInfo() {
           onClick={changeHealthDisplay}
         >
           <Heart
+            style={{ fill: "url(#health-gradient)" }}
             strokeWidth={0.7}
             stroke="white"
-            className="size-24 fill-red-600"
+            className="size-24"
           />
           {healthDisplay === 0 ? (
             <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[70%] font-extrabold text-2xl">
-              {player.pv}
+              {player.pv > 0 ? player.pv : 0}
             </span>
           ) : healthDisplay === 1 ? (
             <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[55%] font-extrabold text-lg">
               <div className="flex justify-center items-center flex-col">
-                {player.pv} <Separator /> {player.pvMax}
+                {player.pv > 0 ? player.pv : 0} <Separator /> {player.pvMax}
               </div>
             </span>
           ) : (
             <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[70%] font-extrabold text-xl">
-              {healthPercentage}%
+              {healthPercentage > 0 ? healthPercentage : 0}%
             </span>
           )}
         </div>
